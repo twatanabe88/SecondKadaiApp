a63826e1e67e734f903bbb1ee9470364205f275a
渡邊翼 <twatanabe@watanabetsubasa-no-macbook-air.local>
2016-03-25 10:24:37 +0900

    commit

---
+++ SecandKadaiApp/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  SecandKadaiApp
+//
+//  Created by 渡邊翼 on 2016/03/23.
+//  Copyright © 2016年 渡邊翼. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var ResultLabel: UILabel!
+        var ResultName:String!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ResultLabel.text = "こんにちは、\(ResultName)です"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecandKadaiApp/ViewController.swift
+++ SecandKadaiApp/ViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+@IBOutlet weak var NameField: UITextField!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+                // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        resultViewController.ResultName = NameField.text
+        
     }
 
 
